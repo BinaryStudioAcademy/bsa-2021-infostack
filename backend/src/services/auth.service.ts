1f24d5de7f1634e355d173f90f22cb8aba81cef3
ursarik <chudak.max@gmail.com>
2021-07-29 10:08:29 +0300

    feat: implement login

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -4,6 +4,7 @@ import {
   HttpCode,
   IRegister,
   IUserWithTokens,
+  ILogin,
 } from 'infostack-shared';
 import { generateAccessToken } from '../common/utils/generate-access-token.util';
 import UserRepository from '../data/repositories/user.repository';
@@ -26,4 +27,35 @@ export const register = async (
   delete user.password;
 
   return { ...user, accessToken: generateAccessToken(user.id) };
+};
+
+export const login = async (
+  body: ILogin,
+): Promise<Omit<IUserWithTokens, 'refreshToken'>> => {
+  const userRepository = getCustomRepository(UserRepository);
+  const { email } = body;
+
+  /**
+   * TODO: when ticket with entities will be merged replace
+   * const [user] = await userRepository.find();
+   * with
+   * const user = await userRepository.findByEmail(email);
+   */
+  const [user] = await userRepository.find();
+  if (!user) {
+    throw new HttpError({
+      status: HttpCode.NOT_FOUND,
+      message: 'No user with such email',
+    });
+  }
+
+  // TODO: when ticket with entities will be merged uncomment code below
+  // if (user.password !== password) {
+  //   throw new HttpError({
+  //     status: HttpCode.BAD_REQUEST,
+  //     message: 'Invalid password',
+  //   });
+  // }
+
+  return { ...user, email, accessToken: generateAccessToken(user.id) };
 };
