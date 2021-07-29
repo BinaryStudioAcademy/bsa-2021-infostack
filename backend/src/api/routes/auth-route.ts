211011dab11f8d9686ce2777a7581479367381c5
ursarik <chudak.max@gmail.com>
2021-07-29 10:09:03 +0300

    feat: add auth/login route

--- backend/src/api/routes/auth-route.ts
+++ backend/src/api/routes/auth-route.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import { run } from '../../common/helpers/route.helper';
-import { register } from '../../services/auth.service';
+import { login, register } from '../../services/auth.service';
 
 const router: Router = Router();
 
 router.post(
   '/register',
   run((req) => register(req.body)),
+);
+
+router.post(
+  '/login',
+  run((req) => login(req.body)),
 );
 
 export default router;
