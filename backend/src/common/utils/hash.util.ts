13f672e1c28d86ea602d9c05cae29e22693e2681
ursarik <chudak.max@gmail.com>
2021-07-29 13:43:12 +0300

    fix: param name

--- backend/src/common/utils/hash.util.ts
+++ backend/src/common/utils/hash.util.ts
@@ -3,16 +3,16 @@ import { promisify } from 'util';
 
 const scrypt = promisify(crypto.scrypt);
 
-const hash = async (password: string): Promise<string> => {
+const hash = async (value: string): Promise<string> => {
   const salt = crypto.randomBytes(8).toString('hex');
-  const derivedKey = (await scrypt(password, salt, 64)) as Buffer;
+  const derivedKey = (await scrypt(value, salt, 64)) as Buffer;
   return salt + ':' + derivedKey.toString('hex');
 };
 
-const verify = async (password: string, hash: string): Promise<boolean> => {
+const verify = async (value: string, hash: string): Promise<boolean> => {
   const [salt, key] = hash.split(':');
   const keyBuffer = Buffer.from(key, 'hex');
-  const derivedKey = (await scrypt(password, salt, 64)) as Buffer;
+  const derivedKey = (await scrypt(value, salt, 64)) as Buffer;
   return crypto.timingSafeEqual(keyBuffer, derivedKey);
 };
 
