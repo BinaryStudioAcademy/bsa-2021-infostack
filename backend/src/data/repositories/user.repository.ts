f0a15e0d73d14a7b3f90ae64949072bd04597c4d
ursarik <chudak.max@gmail.com>
2021-07-29 10:07:46 +0300

    fix: fix return type & comment

--- backend/src/data/repositories/user.repository.ts
+++ backend/src/data/repositories/user.repository.ts
@@ -3,9 +3,9 @@ import { User } from '../entities/user';
 
 @EntityRepository(User)
 class UserRepository extends Repository<User> {
-  public async findByEmail(_: string): Promise<null> {
+  public async findByEmail(_: string): Promise<null | User> {
     /**
-     *  TODO: when ticket with entities will be merged replace
+     *  TODO: when ticket with entities will be merged fix return type replace
      *  _ with email
      *  return null with return this.findOne({ email });
      * */
