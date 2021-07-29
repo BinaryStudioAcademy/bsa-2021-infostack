6317d4fa9cbe6562978410db8d154958bd3546ff
ursarik <chudak.max@gmail.com>
2021-07-29 09:33:57 +0300

    feat: add findByEmail method

--- backend/src/data/repositories/user.repository.ts
+++ backend/src/data/repositories/user.repository.ts
@@ -3,7 +3,14 @@ import { User } from '../entities/user';
 
 @EntityRepository(User)
 class UserRepository extends Repository<User> {
-
+  public async findByEmail(_: string): Promise<null> {
+    /**
+     *  TODO: when ticket with entities will be merged replace
+     *  _ with email
+     *  return null with return this.findOne({ email });
+     * */
+    return null;
+  }
 }
 
 export default UserRepository;
