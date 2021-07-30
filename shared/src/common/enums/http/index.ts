f888fb9b361fd0d6d5a0eadc08962698b54d3830
ursarik <chudak.max@gmail.com>
2021-07-30 12:28:26 +0300

    feat: add HttpErrorMessage enum

--- shared/src/common/enums/http/index.ts
+++ shared/src/common/enums/http/index.ts
@@ -1,3 +1,4 @@
 export { HttpCode } from './http-code.enum';
 export { HttpMethod } from './http-method.enum';
 export { HttpHeader } from './http-header.enum';
+export { HttpErrorMessage } from './http-error-message.enum';
