79cc5b2b217d177cf195643d86fe17576b6105a5
schernaya <chernaya.sofiia@gmail.com>
2021-07-30 23:34:20 +0300

    fix: delete delete components placeholders

--- frontend/src/components/workspace/team-settings/team-settings.tsx
+++ frontend/src/components/workspace/team-settings/team-settings.tsx
@@ -1,18 +1,7 @@
-import { TabPane } from 'react-bootstrap';
-
 const TeamSettings: React.FC = () => {
   return (
     <>
       <p className="content-header">Teams</p>
-      <TabPane role="tabpanel">
-        <p className="settings-content">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Nihil odit magnam minima, soluta doloribus reiciendis
-          molestiae placeat unde eos molestias. Quisquam aperiam,
-          pariatur. Tempora, placeat ratione porro voluptate odit
-          minima.
-        </p>
-      </TabPane>
     </>
   );
 };
