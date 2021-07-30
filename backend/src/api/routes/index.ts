e7c9100b7e50adb59fb81f04a7338e3ef07adbd1
schernaya <chernaya.sofiia@gmail.com>
2021-07-30 13:46:44 +0300

    fix: delete unecessary router

--- backend/src/api/routes/index.ts
+++ backend/src/api/routes/index.ts
@@ -1,10 +1,8 @@
 import { Express } from 'express';
 import userRoute from './user-route';
-import workspaceRoute from './workspace-route';
 
 const routes = (app: Express): void => {
   app.use('/api/users', userRoute);
-  app.use('/api/workspace', workspaceRoute);
 };
 
 export default routes;
