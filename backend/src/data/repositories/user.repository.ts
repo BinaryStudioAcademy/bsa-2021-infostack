a2c6752b1d67b7989ee066587d7d0a2220f602bb
AndriiPopov3 <66309913+AndriiPopov3@users.noreply.github.com>
2021-07-30 16:22:56 +0300

    feat: added repositories

--- backend/src/data/repositories/user.repository.ts
+++ backend/src/data/repositories/user.repository.ts
@@ -3,7 +3,9 @@ import { User } from '../entities/user';
 
 @EntityRepository(User)
 class UserRepository extends Repository<User> {
-
+  findById(id: string):Promise<User> {
+    return this.findOne({ id });
+  }
 }
 
 export default UserRepository;
