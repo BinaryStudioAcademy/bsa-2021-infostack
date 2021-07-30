2e6321ed44d49add290c54d671e127c938ee8459
ursarik <chudak.max@gmail.com>
2021-07-30 12:27:37 +0300

    feat: log error in error handler

--- backend/src/api/middlewares/error-handler-middleware.ts
+++ backend/src/api/middlewares/error-handler-middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpCode } from '../../common/enums/http-code';
 import { HttpError } from '../../common/errors/http-error';
+import { logger } from '../../common/utils/logger.util';
 
 const errorHandlerMiddleware = (
   err: HttpError,
@@ -8,13 +9,17 @@ const errorHandlerMiddleware = (
   res: Response,
   __: NextFunction,
 ): void => {
+  let status = HttpCode.INTERNAL_SERVER_ERROR;
+  let message = 'Internal Server Error';
+
   if (err instanceof HttpError) {
-    res.status(err.status).send({ error: err.message });
-  } else {
-    res
-      .status(HttpCode.INTERNAL_SERVER_ERROR)
-      .send({ error: 'Internal Server Error' });
+    status = err.status;
+    message = err.message;
   }
+
+  logger.error({ status, message });
+
+  res.status(status).send({ error: message });
 };
 
 export default errorHandlerMiddleware;
