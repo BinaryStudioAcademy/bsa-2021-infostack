610915134eec837ba3f34740f4f95048f1dc2c78
Mykyta Batrak <fighter662@gmail.com>
2021-08-02 19:15:47 +0300

    fix: authMiddleware correctly extracts workspaceId

--- backend/src/api/middlewares/authorization-middleware.ts
+++ backend/src/api/middlewares/authorization-middleware.ts
@@ -13,20 +13,13 @@ export const auth = (
     return next();
   }
 
-  const { workspaceId } = req.cookies;
-
-  if (workspaceId) {
-    req.workspaceId = workspaceId;
-    next();
-  } else {
-    res.status(HttpCode.NOT_FOUND).json({ msg: 'Workspace is not found' });
-    throw new Error('Workspace is not found');
-  }
-
   try {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     const decoded = jwt.verify(token, process.env.SECRET_KEY) as string;
+    const { workspaceId } = req.cookies;
+
+    req.workspaceId = workspaceId;
     req.userId = decoded;
     next();
   } catch (err) {
--- backend/src/api/routes/index.ts
+++ backend/src/api/routes/index.ts
@@ -1,11 +1,9 @@
 import { Express } from 'express';
 import userRoute from './user-route';
 import authRoute from './auth-route';
-// import { auth } from '../middlewares/authorization-middleware';
-// import { checkWorkspace } from '../middlewares/check-workspace-middleware';
 
 const routes = (app: Express): void => {
-  app.use('/api/users',  userRoute);
+  app.use('/api/users', userRoute);
   app.use('/api/auth', authRoute);
 };
 
