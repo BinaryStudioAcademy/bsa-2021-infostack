b51a6145cb514716013a51d2141f578de29fbd90
ursarik <chudak.max@gmail.com>
2021-08-02 15:42:48 +0300

    feat: workspace store

--- frontend/src/store/root-reducer.ts
+++ frontend/src/store/root-reducer.ts
@@ -1,9 +1,11 @@
 import { counterReducer as counter } from './counter';
 import { authReducer as auth } from './auth';
+import { workspaceReducer as workspace } from './workspace';
 
 const rootReducer = {
   counter,
   auth,
+  workspace,
 };
 
 export { rootReducer };
