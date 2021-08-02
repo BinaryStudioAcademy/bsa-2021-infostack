385739690d125b83e6b49e618b809a9376e3d2bb
ursarik <chudak.max@gmail.com>
2021-08-02 17:36:09 +0300

    fix: update args name

--- frontend/src/components/workspace/users-settings/user-item/user-item.tsx
+++ frontend/src/components/workspace/users-settings/user-item/user-item.tsx
@@ -2,16 +2,16 @@ import { getAllowedClasses } from '../../../../helpers/dom/dom';
 import styles from '../styles.module.scss';
 import { IWorkspaceUser } from '../../../../common/interfaces/workspace';
 import { AddRole } from './add-role/add-role';
-import { Team } from './team/team';
+import { Teams } from './teams/teams';
 
 interface IUserItemProps extends IWorkspaceUser {}
 
-const UserItem: React.FC<IUserItemProps> = ({ name, role, team }) => {
+const UserItem: React.FC<IUserItemProps> = ({ fullName, role, teams }) => {
   return (
     <tr>
-      <td>{name}</td>
+      <td>{fullName}</td>
       <td>{role}</td>
-      {team ? <Team team={team} /> : <AddRole />}
+      {teams.length ? <Teams teams={teams} /> : <AddRole />}
       <td>
         <i className="bi-pencil" />
         <i className={getAllowedClasses('bi-trash', styles.trashIcon)} />
