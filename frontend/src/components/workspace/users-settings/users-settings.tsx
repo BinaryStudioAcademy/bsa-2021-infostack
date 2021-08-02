8008e966ec659f819ae8488226f4ddd3457cc45f
ursarik <chudak.max@gmail.com>
2021-08-02 15:44:43 +0300

    feat: get users from store. on load dispatch loadUsers

--- frontend/src/components/workspace/users-settings/users-settings.tsx
+++ frontend/src/components/workspace/users-settings/users-settings.tsx
@@ -3,21 +3,29 @@ import AddUser from './add-user/add-user';
 import styles from './styles.module.scss';
 import TableHead from './table-head/table-head';
 import UserItem from './user-item/user-item';
+import {
+  useEffect,
+  useAppDispatch,
+  useAppSelector,
+} from '../../../hooks/hooks';
+import { workspaceActions } from '../../../store/workspace';
 
 export const TABLE_HEADERS = ['Name', 'Workspace Role', 'Team', 'Actions'];
-// TODO: use api data
-const USERS = [
-  { id: 1, name: 'Some name', role: 'admin', team: 'Team 1' },
-  { id: 2, name: 'Some name', role: 'admin', team: 'Team 1' },
-];
 
 const UsersSettings: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const users = useAppSelector((state) => state.workspace.users);
+
+  useEffect(() => {
+    dispatch(workspaceActions.loadUsers());
+  }, []);
+
   return (
     <div className={styles.userSettingsContainer}>
       <Table hover>
         <TableHead headers={TABLE_HEADERS} />
         <tbody>
-          {USERS.map((user) => {
+          {users.map((user) => {
             return <UserItem key={user.id} {...user} />;
           })}
           <AddUser />
