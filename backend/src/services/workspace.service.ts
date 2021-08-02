b259e597455046b6a36b8e26dd6166b02a4dfe78
ursarik <chudak.max@gmail.com>
2021-08-02 17:33:21 +0300

    fix: remove mock

--- backend/src/services/workspace.service.ts
+++ backend/src/services/workspace.service.ts
@@ -1,12 +1,13 @@
-import { RoleType } from 'infostack-shared';
+import { getCustomRepository } from 'typeorm';
 import { IWorkspaceUser } from '../common/interfaces/workspace/workspace-user';
+import { mapWorkspaceToWorkspaceUsers } from '../common/mappers/workspace/map-workspace-to-workspace-users';
+import WorkspaceRepository from '../data/repositories/workspace.repository';
 
 export const getUsers = async (
   workspaceId: string,
 ): Promise<IWorkspaceUser[]> => {
-  // TODO: remove mock
-  return [
-    { id: workspaceId, name: 'Some name', role: RoleType.ADMIN },
-    { id: '2', name: 'Some name', role: RoleType.ADMIN, team: 'Team 1' },
-  ];
+  const workspaceRepository = getCustomRepository(WorkspaceRepository);
+  const workspace = await workspaceRepository.findByIdWithUsers(workspaceId);
+
+  return mapWorkspaceToWorkspaceUsers(workspace);
 };
