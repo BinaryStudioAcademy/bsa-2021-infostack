b4430d194fb83e1fd67ddd4febc3bec9867a5329
ursarik <chudak.max@gmail.com>
2021-08-02 19:01:32 +0300

    fix: Use template literal. Remove method

--- frontend/src/services/workspace-api/workspace-api.service.ts
+++ frontend/src/services/workspace-api/workspace-api.service.ts
@@ -1,5 +1,5 @@
 import { IWorkspaceUser } from 'common/interfaces/workspace';
-import { HttpMethod, ContentType } from 'common/enums/enums';
+import { ContentType } from 'common/enums/enums';
 import { Http } from 'services/http/http.service';
 
 const http = new Http();
@@ -8,8 +8,7 @@ class WorkspaceApi {
   private BASE = '/api/workspaces/';
 
   public async loadUsers(): Promise<IWorkspaceUser[]> {
-    const response: IWorkspaceUser[] = await http.load(this.BASE + 'users', {
-      method: HttpMethod.GET,
+    const response: IWorkspaceUser[] = await http.load(`${this.BASE}/users`, {
       contentType: ContentType.JSON,
     });
 
