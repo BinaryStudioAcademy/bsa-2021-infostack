6f6a141983f45674bfa9af6dde8f5330bf37839e
neonila-kunyk <neonila.kunik@gmail.com>
2021-08-03 11:05:35 +0300

    fix: remove uuid

--- backend/src/services/workspace.service.ts
+++ backend/src/services/workspace.service.ts
@@ -1,5 +1,4 @@
 import { getCustomRepository } from 'typeorm';
-import { v4 as uuidv4 } from 'uuid';
 import { IWorkspace, IWorkspaceCreation } from 'infostack-shared/common/interfaces';
 import UserRepository from '../data/repositories/user.repository';
 import WorkspaceRepository from '../data/repositories/workspace.repository';
@@ -22,8 +21,7 @@ export const create = async (userId: string, data: IWorkspaceCreation): Promise<
   const userWorkspaceRepository = getCustomRepository(UserWorkspaceRepository);
   const userRepository = getCustomRepository(UserRepository);
   const user = await userRepository.findById(userId);
-  const id = uuidv4();
-  const workspace = workspaceRepository.create({ id, name: data.title });
+  const workspace = workspaceRepository.create({ name: data.title });
   await workspaceRepository.save(workspace);
   const userWorkspace = userWorkspaceRepository.create({ user, workspace, role: UserRole.ADMIN });
   await userWorkspaceRepository.save(userWorkspace);
