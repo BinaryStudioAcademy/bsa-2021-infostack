6dbfcd43d05d2e6e64bd7887cf37eb5741cac4d1
sandrk <oleksandr.kukharenko@binary-studio.com>
2021-08-03 23:54:39 +0300

    fix: do not use hardcoded hash in seeder

--- backend/src/data/seed-data/user.data.ts
+++ backend/src/data/seed-data/user.data.ts
@@ -1,18 +1,18 @@
+const password = 'admin123';
+
 export const users = [
   {
     id: '9a25ad37-a647-4c1a-897f-59dbf275765d',
     fullName: 'User1',
     email: 'example.email@example.com',
     avatar: '',
-    password:
-      '2ff484ab573e6567:346f9530144f57c11f038cc91b154dd0ece1ac7f19215f051bafac63f3f60771abf7d0cef2db4ff4ca5046c5278c22efeac941e10aa0efd850423f1a64ea67b3',
+    password,
   },
   {
     id: 'c5fa3b2f-c4de-4dda-84e7-714ee852627e',
     fullName: 'User2',
     email: 'example2.email@example.com',
     avatar: '',
-    password:
-      '2ff484ab573e6567:346f9530144f57c11f038cc91b154dd0ece1ac7f19215f051bafac63f3f60771abf7d0cef2db4ff4ca5046c5278c22efeac941e10aa0efd850423f1a64ea67b3',
+    password,
   },
 ];
