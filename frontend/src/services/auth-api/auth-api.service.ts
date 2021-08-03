04f1bbfbc87a9b6715bc1d9c2a9d665d13394cd4
ursarik <chudak.max@gmail.com>
2021-08-03 20:45:24 +0300

    feat: AuthApi.setPassword

--- frontend/src/services/auth-api/auth-api.service.ts
+++ frontend/src/services/auth-api/auth-api.service.ts
@@ -1,7 +1,7 @@
 import { IUserWithTokens, IUser } from 'common/interfaces/user';
 import { HttpMethod, ContentType } from 'common/enums/enums';
 import { Http } from 'services/http/http.service';
-import { IResetPassword } from 'common/interfaces/auth';
+import { IResetPassword, ISetPassword } from 'common/interfaces/auth';
 
 const http = new Http();
 
@@ -35,6 +35,14 @@ class AuthApi {
 
   public async resetPassword(payload: IResetPassword): Promise<void> {
     return http.load('/api/auth/reset-password', {
+      method: HttpMethod.POST,
+      payload: JSON.stringify(payload),
+      contentType: ContentType.JSON,
+    });
+  }
+
+  public async setPassword(payload: ISetPassword): Promise<void> {
+    return http.load('/api/auth/set-password', {
       method: HttpMethod.POST,
       payload: JSON.stringify(payload),
       contentType: ContentType.JSON,
