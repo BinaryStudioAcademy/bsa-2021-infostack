c2173c94d1cc2c4aacfb56554270b6b2c685f979
neonila-kunyk <neonila.kunik@gmail.com>
2021-08-03 22:13:32 +0300

    fix: fix style

--- frontend/src/components/workspaces/workspaces.tsx
+++ frontend/src/components/workspaces/workspaces.tsx
@@ -57,10 +57,7 @@ const Workspaces: React.FC = () => {
   };
 
   return (
-    <div
-      className={
-        `workspaces text-secondary bg-light d-flex flex-column align-items-start p-4${!workspaces ? ' vh-91' : ''}`
-      }>
+    <div className="workspaces text-secondary bg-light d-flex flex-column align-items-start p-4 vh-91">
       <h1 className="pageTitle">Workspaces</h1>
       {
         !workspaces && <div className="d-flex flex-grow-1 align-items-center justify-content-center w-100">
