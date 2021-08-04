8dab5a9e1e05912ebff7e9ac1a3bcb87bdf739b5
ursarik <chudak.max@gmail.com>
2021-08-04 15:10:29 +0300

    fix: use react toastify

--- frontend/src/components/reset-password/reset-password.tsx
+++ frontend/src/components/reset-password/reset-password.tsx
@@ -2,13 +2,11 @@ import Sign from 'components/common/sign/sign';
 import FormField from 'components/common/form-field/form-field';
 import { useState } from 'hooks/hooks';
 import { containsNoEmptyStrings } from 'helpers/helpers';
-import { Alert } from 'react-bootstrap';
-import styles from './styles.module.scss';
 import { AuthApi } from '../../services';
+import { toast } from 'react-toastify';
 
 const ResetPassword: React.FC = () => {
   const [email, setEmail] = useState('');
-  const [showAlert, setShowAlert] = useState(false);
   const [isSendingMail, setIsSendingMail] = useState(false);
 
   const handleSubmit = async (e: React.SyntheticEvent): Promise<void> => {
@@ -22,17 +20,16 @@ const ResetPassword: React.FC = () => {
 
     await new AuthApi().resetPassword({ email });
 
-    setShowAlert(true);
+    toast.info(
+      'We sent you an email to reset your password. Please check your mailbox.',
+    );
+
     setEmail('');
     setIsSendingMail(false);
   };
 
   const onEmailChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setEmail(event.target.value);
-  };
-
-  const onAlertClose = (): void => {
-    setShowAlert(false);
   };
 
   return (
@@ -52,16 +49,6 @@ const ResetPassword: React.FC = () => {
         value={email}
         onChange={onEmailChange}
       />
-
-      <Alert
-        className={styles.alert}
-        variant={'primary'}
-        onClose={onAlertClose}
-        dismissible
-        show={showAlert}
-      >
-        We sent you an email to reset your password. Please check your mailbox.
-      </Alert>
     </Sign>
   );
 };
