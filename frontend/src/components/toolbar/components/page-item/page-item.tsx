cddedb5096850406312de6fa975378936e8707fc
ovchynnikov <slame@meta.ua>
2021-08-05 21:27:05 +0300

    commit before pull

--- frontend/src/components/toolbar/components/page-item/page-item.tsx
+++ frontend/src/components/toolbar/components/page-item/page-item.tsx
@@ -1,14 +1,13 @@
 import{ Accordion } from 'react-bootstrap';
 import { Link } from 'components/common/common';
 import { getAllowedClasses } from 'helpers/dom/dom';
-import PlusButton from '../plus-button/plus-button';
 import { IPage } from 'common/interfaces/page';
 import { AppRoute } from 'common/enums/enums';
 import styles from '../../styles.module.scss';
 import { useAppDispatch } from 'hooks/hooks';
 import { pagesActions } from 'store/pages';
-import Button from 'react-bootstrap/Button';
 import { IPageRequest } from 'common/interfaces/pages';
+import PlusButtonRoot from '../plus-button/plus-button-root';
 
 type Props = {
   title?: string;
@@ -41,18 +40,18 @@ const PageItem: React.FC<Props> = ({ title = 'default', id, childPages }) => {
               <Accordion.Header className={styles.accordionHeader}>
                 <div className={getAllowedClasses('d-flex w-100 justify-content-between align-items-center', styles.pageItem)}>
                   <Link onClick={(): Promise<void> => getPageById(id)} to={AppRoute.PAGES} className={getAllowedClasses(styles.navbarBrand, styles.navbarLinkInsideSection, 'd-flex')}>{title}</Link>
-                  {!childPages && <span className={getAllowedClasses('px-2',styles.plus)}><PlusButton id={id}/></span>}
+                  {!childPages && <span onClick={(): Promise<void> => addSubPage(id)} className={getAllowedClasses('px-2',styles.plus)}><PlusButtonRoot/></span>}
                 </div>
+                <span onClick={(): Promise<void> => addSubPage(id)} className={getAllowedClasses('px-3', styles.plus)}><PlusButtonRoot/></span>
               </Accordion.Header>
               <Accordion.Body className={styles.accordionBody}>
-                {childPages && <Button variant="primary" onClick={(): Promise<void> => addSubPage(id)} size="sm">Add page</Button>}
                 {childPages && childPages.map(({ pageContents, id, childPages }) => <PageItem id={id} key={id} title={pageContents[0]?.title} childPages={childPages} />)}
 
               </Accordion.Body>
             </> :
             <div className={getAllowedClasses('d-flex justify-content-between align-items-center', styles.pageItem)}>
               <Link onClick={(): Promise<void> => getPageById(id)} to={AppRoute.PAGES} className={getAllowedClasses(styles.navbarBrand, styles.navbarLinkInsideSection, 'd-flex')}>{title}</Link>
-              <span className={getAllowedClasses('px-2',styles.plus)}><PlusButton id={id}/></span>
+              <span onClick={(): Promise<void> => addSubPage(id)} className={getAllowedClasses('px-2',styles.plus)}><PlusButtonRoot/></span>
             </div>}
         </Accordion.Item>
       </Accordion>
