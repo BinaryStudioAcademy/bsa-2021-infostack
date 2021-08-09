74e6d84b75e5f7e2cd375c51b319f4077a7dcde8
neonila-kunyk <neonila.kunik@gmail.com>
2021-08-10 00:47:59 +0300

    fix: fix load user

--- frontend/src/components/main/main.tsx
+++ frontend/src/components/main/main.tsx
@@ -1,4 +1,4 @@
-import { AppRoute, CookieVariable } from 'common/enums/enums';
+import { AppRoute, CookieVariable, LocalStorageVariable } from 'common/enums/enums';
 import { Switch } from 'components/common/common';
 import ProtectedRoute from 'components/common/protected-route/protected-route';
 import Header from 'components/header/header';
@@ -9,17 +9,31 @@ import ProfileInfo from 'components/profile-info/profile-info';
 import Workspace from 'components/workspace/workspace';
 import styles from './styles.module.scss';
 import Settings from 'components/settings/settings';
-import { useAppSelector, useAppDispatch, useHistory, useCookies } from 'hooks/hooks';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useEffect,
+  useHistory,
+  useCookies,
+} from 'hooks/hooks';
+import { authActions } from 'store/actions';
 import { workspacesActions } from 'store/actions';
-import { useEffect } from 'react';
 
 const Main: React.FC = () => {
   const { currentWorkspace } = useAppSelector((state) => state.workspaces);
+  const { user } = useAppSelector(state => state.auth);
   const history = useHistory();
   const dispatch = useAppDispatch();
   const [cookies] = useCookies([
     CookieVariable.WORKSPACE_ID,
   ]);
+  const token = localStorage.getItem(LocalStorageVariable.ACCESS_TOKEN);
+
+  useEffect(() => {
+    if (token && !user){
+      dispatch(authActions.loadUser());
+    }
+  }, []);
 
   useEffect(() => {
     if (!currentWorkspace) {
