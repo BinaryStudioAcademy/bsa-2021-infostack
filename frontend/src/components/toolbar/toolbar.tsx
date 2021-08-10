2fdb4a7c614badd445b9de71f1c8cebdf843f10c
ursarik <chudak.max@gmail.com>
2021-08-10 14:47:55 +0300

    fix: multiple page loading after refresh

--- frontend/src/components/toolbar/toolbar.tsx
+++ frontend/src/components/toolbar/toolbar.tsx
@@ -1,6 +1,11 @@
 import { RootState } from 'common/types/types';
 import { getAllowedClasses } from 'helpers/dom/dom';
-import { useAppDispatch, useAppSelector, useEffect, useHistory } from 'hooks/hooks';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useEffect,
+  useHistory,
+} from 'hooks/hooks';
 import { Accordion, Navbar } from 'react-bootstrap';
 import { pagesActions } from 'store/pages';
 import PagesList from './components/pages-list/pages-list';
@@ -22,32 +27,41 @@ const Toolbar: React.FC<Props> = ({ title = 'Untitled' }) => {
     dispatch(pagesActions.getPagesAsync());
   }, []);
 
-  const addPage = async(): Promise<void> => {
+  const addPage = async (): Promise<void> => {
     const payload: IPageRequest = { title: 'New Page', content: '' };
     await dispatch(pagesActions.createPage(payload));
     await dispatch(pagesActions.getPagesAsync());
   };
 
   useEffect(() => {
-    history.push(`${AppRoute.PAGE.slice(0, AppRoute.PAGE.length - 3)}${currentPage?.id}`);
-  },[currentPage]);
+    if (currentPage?.id) {
+      history.push(
+        `${AppRoute.PAGE.slice(0, AppRoute.PAGE.length - 3)}${currentPage?.id}`,
+      );
+    }
+  }, [currentPage?.id]);
 
   const pages = useAppSelector((state: RootState) => state.pages);
 
-  const SectionName: React.FC<{ name: string }> = ({ name }) =>
-    <h2 className={getAllowedClasses(styles.sectionName)}>{name}</h2>;
+  const SectionName: React.FC<{ name: string }> = ({ name }) => (
+    <h2 className={getAllowedClasses(styles.sectionName)}>{name}</h2>
+  );
 
   return (
     <Navbar className="bg-dark flex-column px-5 overflow-auto w-100 vh-100">
-      <h1 className="h5 mt-5 mb-5 text-light text-center">
-        {title}
-      </h1>
+      <h1 className="h5 mt-5 mb-5 text-light text-center">{title}</h1>
       <div className="pt-3 w-100">
         <div className="pt-3 w-100 pt-3 w-100 d-flex justify-content-between align-items-center">
-          <SectionName name="Pages" /><span onClick={addPage} className={getAllowedClasses('px-0', styles.plusRoot)}><PlusButtonRoot/></span>
+          <SectionName name="Pages" />
+          <span
+            onClick={addPage}
+            className={getAllowedClasses('px-0', styles.plusRoot)}
+          >
+            <PlusButtonRoot />
+          </span>
         </div>
         <Accordion className={styles.accordion} defaultActiveKey="1" flush>
-          <PagesList pages={pages.pages}/>
+          <PagesList pages={pages.pages} />
         </Accordion>
       </div>
     </Navbar>
