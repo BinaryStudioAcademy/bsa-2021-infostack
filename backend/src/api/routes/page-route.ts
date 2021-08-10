183fc53ecc55120e82e5452fa7eb5340b301bff5
ursarik <chudak.max@gmail.com>
2021-08-10 15:20:09 +0300

    feat: contributors route

--- backend/src/api/routes/page-route.ts
+++ backend/src/api/routes/page-route.ts
@@ -1,16 +1,32 @@
 import { Router } from 'express';
 import { run } from '../../common/helpers/route.helper';
-import { createPage, getPages, getPage } from '../../services/page.service';
+import {
+  createPage,
+  getPages,
+  getPage,
+  getContributors,
+} from '../../services/page.service';
 
 const router: Router = Router();
 
-router
-  .post('/', run(req => createPage(req.userId, req.workspaceId, req.body)));
+router.post(
+  '/',
+  run((req) => createPage(req.userId, req.workspaceId, req.body)),
+);
 
-router
-  .get('/', run(req => getPages(req.userId, req.workspaceId)));
+router.get(
+  '/',
+  run((req) => getPages(req.userId, req.workspaceId)),
+);
 
-router
-  .get('/:id', run(req => getPage(req.workspaceId, req.params.id)));
+router.get(
+  '/:id',
+  run((req) => getPage(req.workspaceId, req.params.id)),
+);
+
+router.get(
+  '/:id/contributors',
+  run((req) => getContributors(req.params.id)),
+);
 
 export default router;
