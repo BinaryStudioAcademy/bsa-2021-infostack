b0af11c777156c534d3387c607beff325f58e0a2
ursarik <chudak.max@gmail.com>
2021-08-10 16:22:14 +0300

    fix: seed data

--- backend/src/data/seed-data/user.data.ts
+++ backend/src/data/seed-data/user.data.ts
@@ -25,7 +25,7 @@ export const users = [
   {
     id: 'bf873638-9388-4298-94b0-3d1a11ef8688',
     fullName: 'User 4',
-    email: 'example3.email@example.com',
+    email: 'example4.email@example.com',
     avatar: '',
     password,
   },
