180fdd68ad5ca6a22069c9cf396425a0472b689d
Denis Klemeshov <denis.klemeshov@gmail.com>
2021-08-10 17:36:43 +0300

    fix: add toolbar title

--- frontend/src/components/common/with-header/with-header.tsx
+++ frontend/src/components/common/with-header/with-header.tsx
@@ -1,17 +1,19 @@
+import React from 'react';
+import { useAppSelector } from 'hooks/hooks';
 import Header from 'components/header/header';
 import Toolbar from 'components/toolbar/toolbar';
-import React from 'react';
 import styles from './styles.module.scss';
 
 const WithHeader = (Component: React.FC): React.FC => {
   const componentWithHeader: React.FC = () => {
+    const { currentWorkspace } = useAppSelector((state) => state.workspaces);
     return (
       <div className={styles.grid}>
         <div className={styles.header}>
           <Header />
         </div>
         <div className={styles.toolbar}>
-          <Toolbar />
+          <Toolbar title={currentWorkspace?.title}/>
         </div>
         <div className={styles.content}>
           <Component />
