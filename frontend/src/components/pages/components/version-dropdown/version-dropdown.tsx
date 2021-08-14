a895d64130b6e68905612729d801cb4a9c07a20d
ovchynnikov <vapore18@gmail.com>
2021-08-14 13:12:00 +0300

    feat: formatted version dates func

--- frontend/src/components/pages/components/version-dropdown/version-dropdown.tsx
+++ frontend/src/components/pages/components/version-dropdown/version-dropdown.tsx
@@ -1,21 +1,58 @@
 import { AppRoute } from 'common/enums/enums';
 import { Dropdown } from 'react-bootstrap';
 import { VersionItem } from '../version-item/version-item';
+import { BlueCircle } from '../version-item/blue-circle/blue-circle';
+import { useAppSelector } from 'hooks/hooks';
+// import { IPageContent } from 'infostack-shared';
 
 const VersionDropdown: React.FC = () => {
+  const pageContents = useAppSelector(
+    (state) => state.pages.currentPage?.pageContents,
+  );
+  const { currentPage } = useAppSelector((state) => state.pages);
+  // eslint-disable-next-line no-console
+  console.log('pageContents', pageContents);
+
+  // eslint-disable-next-line no-console
+  const latestVersion = currentPage?.pageContents[0];
+
+  const formattedVersionDate = (createdAt: string): string => {
+    const formattedDate = new Date(createdAt),
+      year = formattedDate.getFullYear();
+    let day = '' + formattedDate.getDate(),
+      month = '' + (formattedDate.getMonth() + 1);
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    return [day, month, year].join('.');
+  };
+
+  // eslint-disable-next-line no-console
+  console.log(formattedVersionDate);
+
   return (
     <Dropdown className="me-3 d-inline-flex sm">
       <Dropdown.Toggle className="sm" id="dropdown-page-version">
-        Version:
+        Version: {}
       </Dropdown.Toggle>
       <Dropdown.Menu>
-        <VersionItem to={AppRoute.WORKSPACES}>
-          11.11.2011 Dan Abramovvvvvvvv
-        </VersionItem>
-        <VersionItem to={AppRoute.WORKSPACES}>11.11.2011 John Doe</VersionItem>
-        <VersionItem to={AppRoute.WORKSPACES}>11.11.2011 Evan You</VersionItem>
-        <VersionItem to={AppRoute.WORKSPACES}>11.11.2011 Evan You</VersionItem>
-        <VersionItem to={AppRoute.WORKSPACES}>11.11.2011 Evan You</VersionItem>
+        {pageContents ? (
+          pageContents.map((version) => (
+            <>
+              <VersionItem key={version.id} to={AppRoute.PAGE}>
+                {version.createdAt === latestVersion?.createdAt ? (
+                  <>
+                    <BlueCircle /> {formattedVersionDate(version.createdAt)}{' '}
+                    (Latest)
+                  </>
+                ) : (
+                  formattedVersionDate(version.createdAt)
+                )}
+              </VersionItem>
+            </>
+          ))
+        ) : (
+          <p>no versions</p>
+        )}
       </Dropdown.Menu>
     </Dropdown>
   );
