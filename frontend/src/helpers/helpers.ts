1fdfaf759f664e0c3b70ac458fc7c26477b74e43
ovchynnikov <vapore18@gmail.com>
2021-08-14 20:55:40 +0300

    feat: path changed from pages to page

--- frontend/src/helpers/helpers.ts
+++ frontend/src/helpers/helpers.ts
@@ -1,3 +1,4 @@
 export { checkIsOneOf, containsNoEmptyStrings } from './boolean/boolean';
 export { replaceIdParam } from './route/route';
+export { replacePageIdParamAndVersionId } from './route/route';
 export { sortObjByName } from './array/array';
