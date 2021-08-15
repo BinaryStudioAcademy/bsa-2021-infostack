e43b96f5ca5378ac61305e27f6b6fa745515fd00
ovchynnikov <vapore18@gmail.com>
2021-08-15 20:09:36 +0300

    feat: dropdown with fullName

--- frontend/src/components/pages/components/version-dropdown/version-dropdown.tsx
+++ frontend/src/components/pages/components/version-dropdown/version-dropdown.tsx
@@ -2,17 +2,23 @@ import { Dropdown } from 'react-bootstrap';
 import { VersionItem } from '../version-item/version-item';
 import { BlueCircle } from '../version-item/blue-circle/blue-circle';
 import { useAppSelector, useParams } from 'hooks/hooks';
-import { IPageContent } from 'infostack-shared';
-import styles from './styles.module.scss';
+import { IPageContent, IPageContentWithAuthor } from 'infostack-shared';
 import { getAllowedClasses } from 'helpers/dom/dom';
 import NavLink from 'react-bootstrap/NavLink';
+import { replaceIdParam } from 'helpers/helpers';
+import { AppRoute } from 'common/enums/enums';
+import { useHistory } from 'react-router-dom';
+import { IPageContributor } from 'common/interfaces/pages';
+import Avatar from 'react-avatar';
 
 type Props = {
   currContent?: IPageContent | undefined;
+  contributors: IPageContributor[];
 };
 
-const VersionDropdown: React.FC<Props> = ({ currContent }) => {
+const VersionDropdown: React.FC<Props> = ({ currContent, contributors }) => {
   const pageId = useParams<{ id: string }>().id;
+  const history = useHistory();
 
   const pageContents = useAppSelector(
     (state) => state.pages.currentPage?.pageContents,
@@ -20,6 +26,7 @@ const VersionDropdown: React.FC<Props> = ({ currContent }) => {
   const { currentPage } = useAppSelector((state) => state.pages);
   // eslint-disable-next-line no-console
   console.log('VersionDropdown pageId', pageId);
+
   const currVersionId = useParams<{ versionId: string }>().versionId;
 
   const latestVersion = currentPage?.pageContents[0];
@@ -34,46 +41,119 @@ const VersionDropdown: React.FC<Props> = ({ currContent }) => {
     return [day, month, year].join('.');
   };
 
+  const handleAvatarClick = (userId: string): void => {
+    history.push(replaceIdParam(AppRoute.PROFILE, userId));
+  };
+
+  const pageContentsCopy: IPageContentWithAuthor[] = JSON.parse(
+    JSON.stringify(pageContents),
+  );
+  // eslint-disable-next-line no-console
+  console.log(
+    'contributors',
+    contributors,
+    'pageContentsCopy',
+    pageContentsCopy,
+  );
+
+  pageContentsCopy.forEach((contentVersion: IPageContent, index): void => {
+    const authorId = contentVersion?.authorId;
+    const contributorInState = contributors.find(
+      (contributor) => authorId === contributor.id,
+    );
+    // eslint-disable-next-line no-console
+    console.log('contributorInState', contributorInState);
+
+    if (contributorInState) {
+      const { avatar, fullName, id } = contributorInState;
+      const versionAuthor = {
+        authorId: id,
+        avatar: avatar || 'unavailable',
+        fullName: fullName || 'unavailable',
+      };
+
+      // const pageVersionIndex = pageContentsCopy.findIndex((version: IPageContent) => version.authorId === id);
+
+      if (index >= 0) {
+        pageContentsCopy[index] = {
+          ...pageContentsCopy[index],
+          author: { ...versionAuthor },
+        };
+        // eslint-disable-next-line no-console
+        // console.log('pageContentsCopy[pageVersionIndex]', pageContentsCopy[pageVersionIndex]);
+      } else {
+        pageContentsCopy[index] = {
+          ...pageContentsCopy[index],
+          author: {
+            authorId: 'unavailable',
+            avatar: 'unavailable',
+            fullName: 'unavailable',
+          },
+        };
+      }
+      // eslint-disable-next-line no-console
+      console.log('pageContentsCopy before Render', pageContentsCopy);
+    }
+  });
+
   const versionButtonValue =
     !currVersionId || currentPage?.pageContents[0]?.id === currVersionId
       ? 'Latest'
       : formattedVersionDate(currContent ? currContent.createdAt : '');
   return (
     <Dropdown as={NavLink} className="me-3 d-inline-flex sm">
-      <Dropdown.Toggle
-        as={NavLink}
-        className={getAllowedClasses('sm', styles.dropdownButton)}
-      >
+      <Dropdown.Toggle as={NavLink} className={getAllowedClasses('sm')}>
         Version: {versionButtonValue}
       </Dropdown.Toggle>
       <Dropdown.Menu>
-        {pageContents ? (
-          pageContents.map((version) => (
-            <>
-              <VersionItem
-                id={version.id}
-                versionId={version.id}
-                key={version.id}
-                pageId={pageId}
-                latest={version.createdAt === latestVersion?.createdAt}
-              >
-                {version.createdAt === latestVersion?.createdAt ? (
-                  <>
-                    {' '}
-                    {currVersionId === version.id || !currVersionId ? (
-                      <BlueCircle />
-                    ) : null}
-                    {formattedVersionDate(version.createdAt)} (Latest)
-                  </>
-                ) : (
-                  <>
-                    {currVersionId === version.id ? <BlueCircle /> : null}
-                    {formattedVersionDate(version.createdAt)}
-                  </>
-                )}
-              </VersionItem>
-            </>
-          ))
+        {pageContentsCopy ? (
+          pageContentsCopy.map(
+            ({ id, createdAt, fullName, avatar, author, authorId }) => (
+              <>
+                <p>{`${author.fullName}`}</p>
+                <VersionItem
+                  id={id}
+                  versionId={id}
+                  key={id}
+                  pageId={pageId}
+                  latest={createdAt === latestVersion?.createdAt}
+                >
+                  {createdAt === latestVersion?.createdAt ? (
+                    <>
+                      {' '}
+                      {currVersionId === id || !currVersionId ? (
+                        <BlueCircle />
+                      ) : null}
+                      {formattedVersionDate(createdAt)} (Latest)
+                      <Avatar
+                        // style={avatarStyles}
+                        key={authorId}
+                        name={author ? author?.fullName : 'not found'}
+                        src={author ? author?.avatar : ''}
+                        round={true}
+                        size="20"
+                        onClick={handleAvatarClick.bind(null, id)}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      {currVersionId === id ? <BlueCircle /> : null}
+                      {formattedVersionDate(createdAt)}
+                      <Avatar
+                        // style={avatarStyles}
+                        key={id}
+                        name={fullName}
+                        src={avatar}
+                        round={true}
+                        size="20"
+                        onClick={handleAvatarClick.bind(null, id)}
+                      />
+                    </>
+                  )}
+                </VersionItem>
+              </>
+            ),
+          )
         ) : (
           <p>no versions</p>
         )}
