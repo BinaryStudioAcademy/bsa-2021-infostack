11f94b94372a11282a2cae91d756369707f54103
AndriiPopov3 <66309913+AndriiPopov3@users.noreply.github.com>
2021-08-16 18:48:25 +0300

    added user permission validation for viewing a specific page

--- frontend/src/components/pages/components/page-content/page-content.tsx
+++ frontend/src/components/pages/components/page-content/page-content.tsx
@@ -64,6 +64,7 @@ export const PageContent: React.FC = () => {
   const paramsId = useParams<{ id: string }>().id;
 
   const isPageAdmin = currentPage?.permission === PermissionType.ADMIN;
+  const canView = currentPage?.permission ? true : false;
   const canEdit =
     currentPage?.permission === PermissionType.ADMIN ||
     currentPage?.permission === PermissionType.WRITE;
@@ -175,81 +176,95 @@ export const PageContent: React.FC = () => {
 
     return (
       <div className="p-4">
-        <Row>
-          <Col xs={2}>
-            <PageTableOfContents headings={TOCHeadings} />
-            <PageTags />
-            <PageContributors className="mt-4" contributors={contributors} />
-          </Col>
-          <Col>
+        {canView ? (
+          <>
             <Row>
-              <Col className="d-flex justify-content-between mb-4">
-                <h1 className="h3 mb-3">{pageTitle || 'New Page'}</h1>
-                <div>
-                  {isPageAdmin && (
-                    <Button
-                      onClick={onAssign}
-                      className={canEdit ? 'me-3' : ''}
+              <Col xs={2}>
+                <PageTableOfContents headings={TOCHeadings} />
+                <PageTags />
+                <PageContributors
+                  className="mt-4"
+                  contributors={contributors}
+                />
+              </Col>
+              <Col>
+                <Row>
+                  <Col className="d-flex justify-content-between mb-4">
+                    <h1 className="h3 mb-3">{pageTitle || 'New Page'}</h1>
+                    <div>
+                      {isPageAdmin && (
+                        <Button
+                          onClick={onAssign}
+                          className={canEdit ? 'me-3' : ''}
+                        >
+                          Assign permissions
+                        </Button>
+                      )}
+                      {canEdit && <EditButton onClick={handleEditing} />}
+                      <Button className="ms-3" onClick={onPageFollow}>
+                        {isCurrentPageFollowed ? 'Unfollow' : 'Follow'}
+                      </Button>
+                    </div>
+                  </Col>
+                </Row>
+                <Row className="mb-4">
+                  <Col>
+                    <Card border="light" className={styles.card}>
+                      <Card.Body className={getAllowedClasses(styles.content)}>
+                        {/* @ts-expect-error see https://github.com/rehypejs/rehype/discussions/63 */}
+                        <ReactMarkdown remarkPlugins={[slug, gfm]}>
+                          {content || 'Empty page'}
+                        </ReactMarkdown>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Card
+                      border="light"
+                      className={getAllowedClasses(styles.card)}
                     >
-                      Assign permissions
-                    </Button>
-                  )}
-                  {canEdit && <EditButton onClick={handleEditing} />}
-                  <Button className="ms-3" onClick={onPageFollow}>
-                    {isCurrentPageFollowed ? 'Unfollow' : 'Follow'}
-                  </Button>
-                </div>
+                      <Card.Header>Comments</Card.Header>
+                      <Card.Body>
+                        <CommentSection pageId={paramsId} />
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Row>
               </Col>
             </Row>
-            <Row className="mb-4">
-              <Col>
-                <Card border="light" className={styles.card}>
-                  <Card.Body className={getAllowedClasses(styles.content)}>
-                    {/* @ts-expect-error see https://github.com/rehypejs/rehype/discussions/63 */}
-                    <ReactMarkdown remarkPlugins={[slug, gfm]}>
-                      {content || 'Empty page'}
-                    </ReactMarkdown>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Card border="light" className={getAllowedClasses(styles.card)}>
-                  <Card.Header>Comments</Card.Header>
-                  <Card.Body>
-                    <CommentSection pageId={paramsId} />
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <Popup
-          query="Users & Teams"
-          isVisible={isPopUpVisible}
-          cancelButton={{
-            text: 'Cancel',
-            onClick: handleAssignCancel,
-          }}
-          inviteButton={{
-            text: 'Add user',
-            onClick: handleAssignConfirm,
-          }}
-        />
-        <InviteModal
-          onModalClose={handleIviteCancel}
-          showModal={isModalVisible}
-        />
-        <FollowModal
-          show={isFollowModalVisible}
-          isFollowing={isCurrentPageFollowed}
-          handler={
-            isCurrentPageFollowed
-              ? handlePageUnfollow(paramsId)
-              : handlePageFollow(paramsId)
-          }
-        />
+            <Popup
+              query="Users & Teams"
+              isVisible={isPopUpVisible}
+              cancelButton={{
+                text: 'Cancel',
+                onClick: handleAssignCancel,
+              }}
+              inviteButton={{
+                text: 'Add user',
+                onClick: handleAssignConfirm,
+              }}
+            />
+            <InviteModal
+              onModalClose={handleIviteCancel}
+              showModal={isModalVisible}
+            />
+            <FollowModal
+              show={isFollowModalVisible}
+              isFollowing={isCurrentPageFollowed}
+              handler={
+                isCurrentPageFollowed
+                  ? handlePageUnfollow(paramsId)
+                  : handlePageFollow(paramsId)
+              }
+            />
+          </>
+        ) : (
+          <h1 className="d-flex justify-content-center">
+            No permission to view the requested page
+          </h1>
+        )}
       </div>
     );
   };
