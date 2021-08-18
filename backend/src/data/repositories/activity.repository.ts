268ca833c5dbcd8c1657a106bbc16579c2755553
ursarik <chudak.max@gmail.com>
2021-08-18 19:09:47 +0300

    fix: indents

--- backend/src/data/repositories/activity.repository.ts
+++ backend/src/data/repositories/activity.repository.ts
@@ -11,14 +11,14 @@ class ActivityRepository {
     select comment.id as id, comment."authorId", "user"."fullName", "user".avatar, (extract(epoch from comment."createdAt"::timestamp)) as "createdAtTimestamp", comment."pageId", text as content, ('comment') as type, p.title, (true) as "isNew" from comment
       inner join "user" on comment."authorId" = "user".id
     left join page_content p on (comment."pageId" = p."pageId"
-		and not exists (
-		select 1 from page_content as p1 where p1."pageId" = comment."pageId" and p1."createdAt" > p."createdAt"
-	  )
-	  )
+and not exists (
+select 1 from page_content as p1 where p1."pageId" = comment."pageId" and p1."createdAt" > p."createdAt"
+)
+)
     union
       select page_content.id as id, page_content."authorId", "user"."fullName", "user".avatar, (extract(epoch from page_content."createdAt"::timestamp)) as "createdAtTimestamp", "pageId", content, ('page') as type, page_content.title, EXTRACT(EPOCH FROM page_content."createdAt"::timestamp - page."createdAt"::timestamp) < 1 as "isNew"  from page_content
       inner join "user" on page_content."authorId" = "user".id
-	  inner join "page" on page_content."pageId" = page.id
+inner join "page" on page_content."pageId" = page.id
     order by "createdAtTimestamp" desc
     ${options.take ? `limit ${options.take}` : ''}
     ${options.skip ? `offset ${options.skip}` : ''}
@@ -52,17 +52,17 @@ class ActivityRepository {
     const result = await this._manager.query(
       `
     select comment.id as id, comment."authorId", "user"."fullName", "user".avatar, (extract(epoch from comment."createdAt"::timestamp)) as "createdAtTimestamp", comment."pageId", text as content, ('comment') as type, p.title, (true) as "isNew" from comment
-   	inner join "user" on comment."authorId" = "user".id
-	  left join page_content p on (comment."pageId" = p."pageId"
-		and not exists (
-		select 1 from page_content as p1 where p1."pageId" = comment."pageId" and p1."createdAt" > p."createdAt"
-		)
-	  )
+inner join "user" on comment."authorId" = "user".id
+left join page_content p on (comment."pageId" = p."pageId"
+and not exists (
+select 1 from page_content as p1 where p1."pageId" = comment."pageId" and p1."createdAt" > p."createdAt"
+)
+)
       where comment."authorId" = $1
     union
     select page_content.id as id, page_content."authorId", "user"."fullName", "user".avatar, (extract(epoch from page_content."createdAt"::timestamp)) as "createdAtTimestamp", "pageId", content, ('page') as type, page_content.title, EXTRACT(EPOCH FROM page_content."createdAt"::timestamp - page."createdAt"::timestamp) < 1 as "isNew"  from page_content
       inner join "user" on page_content."authorId" = "user".id
-	  inner join "page" on page_content."pageId" = page.id
+inner join "page" on page_content."pageId" = page.id
       where page_content."authorId" = $1
     order by "createdAtTimestamp" desc
     ${options.take ? `limit ${options.take}` : ''}
