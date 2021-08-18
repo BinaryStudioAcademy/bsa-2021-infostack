e20feb867e68a46fd1af7cad2f51c086b468820c
ursarik <chudak.max@gmail.com>
2021-08-19 01:53:37 +0300

    fix: linter

--- backend/src/data/repositories/activity.repository.ts
+++ backend/src/data/repositories/activity.repository.ts
@@ -28,9 +28,9 @@ select * from "user" as u
       inner join team_member on u.id = team_member."userId"
       inner join team_permission on team_permission."teamId" = team_member."teamId"
       where u.id = $1 and "pageId" = records."pageId")
-order by "createdAtTimestamp" desc 
-    ${options.take ? `limit ${options.take}` : ''}
-    ${options.skip ? `offset ${options.skip}` : ''}`,
+order by "createdAtTimestamp" desc
+${options.take ? `limit ${options.take}` : ''}
+${options.skip ? `offset ${options.skip}` : ''}`,
       [options.userId],
     );
 
@@ -43,20 +43,16 @@ order by "createdAtTimestamp" desc
       select count(id) from (select comment.id as id, comment."authorId", "user"."fullName", "user".avatar, (extract(epoch from comment."createdAt"::timestamp)) as "createdAtTimestamp", comment."pageId", text as content, ('comment') as type, p.title, (true) as "isNew" from comment
       inner join "user" on comment."authorId" = "user".id
       left join page_content p on (comment."pageId" = p."pageId"
-			and not exists (
-select 1 from page_content as p1 where p1."pageId" = comment."pageId" and p1."createdAt" > p."createdAt"
-)
-)
+      and not exists (select 1 from page_content as p1 where p1."pageId" = comment."pageId" and p1."createdAt" > p."createdAt"))
 union
       select page_content.id as id, page_content."authorId", "user"."fullName", "user".avatar, (extract(epoch from page_content."createdAt"::timestamp)) as "createdAtTimestamp", "pageId", content, ('page') as type, page_content.title, EXTRACT(EPOCH FROM page_content."createdAt"::timestamp - page."createdAt"::timestamp) < 1 as "isNew"  from page_content
       inner join "user" on page_content."authorId" = "user".id
 	    inner join "page" on page_content."pageId" = page.id) as records
-where exists (select from user_permission where "pageId" = records."pageId" and  user_permission."userId" = $1)
-or exists (
+where exists (select from user_permission where "pageId" = records."pageId" and  user_permission."userId" = $1) or exists (
 select * from "user" as u
 inner join team_member on u.id = team_member."userId"
-      inner join team_permission on team_permission."teamId" = team_member."teamId"
-      where u.id = $1 and "pageId" = records."pageId")`,
+inner join team_permission on team_permission."teamId" = team_member."teamId"
+where u.id = $1 and "pageId" = records."pageId")`,
       [userId],
     );
 
