7e27fe2edfa3d6ace8a9c90c4afa352af86f03cf
ovchynnikov <vapore18@gmail.com>
2021-08-18 17:05:59 +0300

    feat: add pictures from PC and paste screenshots

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -34,10 +34,8 @@ io.on(SocketEvents.CONNECTION, socketHandlers);
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, './public')));
-// app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
-// app.use(express.urlencoded({ extended: true }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/', authorizationMiddleware, socketMiddleware(io));
