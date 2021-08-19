2f60b3868087dd0daad08bba8d0c4f655beae597
AlexKapic <alexkapic@gmail.com>
2021-08-19 16:50:50 +0300

    fix lint

--- frontend/src/components/pages/components/page-tags/page-tags.tsx
+++ frontend/src/components/pages/components/page-tags/page-tags.tsx
@@ -68,7 +68,7 @@ const PageTags: React.FC = () => {
       const pageTagsIds = pageTags.map((tag) => tag.id);
 
       if (pageTagsIds.some((id) => !allTagsIds.includes(id))) {
-        toast.error("Error: you can't add new tags. Must choose available.");
+        toast.error('test');
         setIsEditMode(!isEditMode);
         return;
       }
