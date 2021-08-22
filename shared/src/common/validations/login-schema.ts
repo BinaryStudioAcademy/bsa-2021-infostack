f3e49fba103e0146a5897567bd05b2a42b599282
ovchynnikov <vapore18@gmail.com>
2021-08-22 15:00:40 +0300

    login schema validation added

--- shared/src/common/validations/login-schema.ts
+++ shared/src/common/validations/login-schema.ts
@@ -1,15 +1,20 @@
 import * as yup from 'yup';
 import { passwordRegex } from './regex/regex';
 
-export const loginSchema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup
-    .string()
-    .min(6)
-    .max(12)
-    .matches(
-      passwordRegex,
-      'password must consist of latin letters (upper and lower case), numbers, and symbols',
-    )
-    .required(),
-});
+export const loginSchema = yup
+  .object()
+  .strict(true)
+  .required()
+  .noUnknown(true)
+  .shape({
+    email: yup.string().email().required(),
+    password: yup
+      .string()
+      .min(6)
+      .max(12)
+      .matches(
+        passwordRegex,
+        'password must consist of latin letters (upper and lower case), numbers, and symbols',
+      )
+      .required(),
+  });
