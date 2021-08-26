7b3cf24169c348a3fb29930381254cc10904c012
ovchynnikov <vapore18@gmail.com>
2021-08-26 16:12:34 +0300

    feat: owner added to team mappers

--- backend/src/common/mappers/team/map-team-to-iteam.ts
+++ backend/src/common/mappers/team/map-team-to-iteam.ts
@@ -2,12 +2,12 @@ import { ITeam } from '../../interfaces/team';
 import { Team } from '../../../data/entities/team';
 
 export const mapTeamToITeam = (team: Team): ITeam => {
-  const { id, name, users } = team;
+  const { id, name, users, owner } = team;
   const mappedUsers = users.map(({ id, fullName, avatar, userWorkspaces }) => ({
     id,
     fullName,
     avatar,
     roleInWorkspace: userWorkspaces[0].role,
   }));
-  return { id, name, users: mappedUsers };
+  return { id, name, owner, users: mappedUsers };
 };
