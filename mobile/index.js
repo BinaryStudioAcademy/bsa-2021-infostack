434f80c63408b39a2125efb2486ce7e90df729fd
Mykyta Batrak <fighter662@gmail.com>
2021-08-27 17:54:44 +0300

    feat: add simple routing

--- mobile/index.js
+++ mobile/index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './components/app';
+import { App } from './components/app/app';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
