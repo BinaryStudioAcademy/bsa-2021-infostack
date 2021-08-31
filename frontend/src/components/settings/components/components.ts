21dc978d0c9329fe38787c7c55a9c5a6c8ba4231
AndriiPopov3 <66309913+AndriiPopov3@users.noreply.github.com>
2021-08-31 14:38:41 +0300

    added skill management in settings

--- frontend/src/components/settings/components/components.ts
+++ frontend/src/components/settings/components/components.ts
@@ -1,6 +1,7 @@
 export { Menu } from './menu/menu';
 export { ProfileSettings } from './profile-settings/profile-settings';
 export { TagSettings } from './tag-settings/tag-settings';
+export { SkillSettings } from './skill-settings/skill-settings';
 export { TeamSettings } from './team-settings/team-settings';
 export { UsersSettings } from './users-settings/users-settings';
 export { IntegrationSettings } from './integration-settings/integration-settings';
