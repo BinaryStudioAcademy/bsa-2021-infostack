6d8d6523021085a534bf151f4dc3c568f9c5e2c4
ursarik <chudak.max@gmail.com>
2021-09-01 10:53:27 +0300

    feat: enums

--- mobile/src/common/enums/index.ts
+++ mobile/src/common/enums/index.ts
@@ -1,2 +1,6 @@
 export { ReducerName } from './reducer-name.enum';
 export { RequestStatus } from './request-status.enum';
+export { EmitterEvents } from './emitter-events';
+export { HttpHeader, HttpMethod, HttpCode } from './http';
+export { ContentType } from './file';
+export { LocalStorageVariable } from './local-storage';
