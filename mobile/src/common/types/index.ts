032ca45832445de3be419a357c62e61668abec01
ursarik <chudak.max@gmail.com>
2021-09-02 17:23:53 +0300

    feat: rename

--- mobile/src/common/types/index.ts
+++ mobile/src/common/types/index.ts
@@ -1,3 +1,3 @@
 export type { AppDispatch } from './app-dispatch.type';
 export type { RootState } from './root-state.type';
-export type { HttpOptions } from './http-options';
+export type { HttpOptions } from './http-options.type';
