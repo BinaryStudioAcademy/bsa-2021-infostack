d17abd69a0ce8ee3d6c772dabd60d9679925fcb8
ursarik <chudak.max@gmail.com>
2021-09-02 14:51:00 +0300

    feat: login component

--- mobile/src/components/app/app.tsx
+++ mobile/src/components/app/app.tsx
@@ -3,13 +3,32 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { PageList } from '../page-list/page-list';
+import { Login } from 'components/auth';
+import { useAppSelector } from 'hooks';
 
 const Stack = createNativeStackNavigator();
 
-export const App: React.FC = () => (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Page List" component={PageList} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+export const App: React.FC = () => {
+  const { user } = useAppSelector((state) => state.auth);
+  const isSignedIn = !!user?.id;
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {isSignedIn ? (
+          <>
+            <Stack.Screen name="Page List" component={PageList} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="Login"
+              component={Login}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
