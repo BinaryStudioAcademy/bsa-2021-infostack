fd87fba4fd7a8b5157e1a8ee4f259cbecfdce335
neonila-kunyk <neonila.kunik@gmail.com>
2021-09-08 20:16:40 +0300

    fix: fix shared

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,6 +1,5 @@
 export * from './common/enums';
 export * from './common/interfaces';
 export * from './common/validations';
-export * from './common/regex';
 export * from './exceptions';
 export * from './helpers';
